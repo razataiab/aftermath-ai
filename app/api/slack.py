d9ea7dac5dca02ed440475c7e50edade04d33124
Raza Taiab <razataiab25.11@gmail.com>
2025-10-16 20:12:01 +0100

    Added functions for retrieving Slack incident channel conversation and converting User IDs to User names for LLM use. EDGE CASE: Sometimes there will be UserIDs inside messages as well. This will need to be replaced with User names for clarity. TODO: Implement Data models for Incident, Users, and Messages to retrieve Metadata and timestamping

--- app/api/slack.py
+++ app/api/slack.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, BackgroundTasks, HTTPException
 from slack_sdk import WebClient
 from slack_sdk.signature import SignatureVerifier
 from fastapi.responses import JSONResponse
+import requests
 from urllib.parse import parse_qs
 from config import settings
 
@@ -32,15 +33,34 @@ async def handle_slash_command(request: Request, background_tasks: BackgroundTas
     form = {k: v[0] if v else '' for k, v in parsed.items()}
 
     channel_id = form.get("channel_id", "unknown")
-    user_id = form.get("user_id", "unknown")
 
     background_tasks.add_task(run_postmortem_generation, channel_id)
     return JSONResponse(content={"text": "⏳ Generating postmortem report... I'll post it here soon."}, status_code=200)
 
 async def run_postmortem_generation(channel_id: str):
     print(f"Running postmortem generation for channel={channel_id}")
+    await retrieve_slack_chat_history(channel_id)
     await send_postmortem_report(channel_id)
     print("Postmortem generation complete!")
 
 async def send_postmortem_report(channel_id: str):
     message = client.chat_postMessage(channel=channel_id, text="Postmortem generation complete")
+
+async def retrieve_slack_chat_history(channel_id: str):
+    conversation_history = []
+    response = client.conversations_history(channel=channel_id)
+
+    conversation_history = response["messages"]
+
+    for msg in conversation_history:
+        user = msg.get("user")
+        username = await retrieve_slack_user_name(user)
+        text = msg.get("text")
+        print(f"{username}: {text}")
+    
+    return conversation_history
+
+async def retrieve_slack_user_name(userId: str):
+    response = client.users_info(user=userId)
+    user_info = response["user"]
+    return user_info.get("real_name")
